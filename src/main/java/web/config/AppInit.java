dbfc7b0716f72a59fa138c57994b9d214677dd13
Damir <shakirov-damir@mail.ru>
2021-01-07 16:56:50 +0300

    07.01.2021 16.56

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,7 +1,10 @@
 package web.config;
 
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.*;
@@ -36,12 +39,36 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
 
     /* Данный метод служит для решения проблем с кодировкой, фильтр обрабатывает первичные запросы */
 
-    protected Filter[] getServletFilters() {
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setEncoding("UTF-8");
-        characterEncodingFilter.setForceEncoding(true);
-        return new Filter[] {characterEncodingFilter};
+//    protected Filter[] getServletFilters() {
+//        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+//        characterEncodingFilter.setEncoding("UTF-8");
+//        characterEncodingFilter.setForceEncoding(true);
+//        return new Filter[] {characterEncodingFilter};
+//    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        //create the root Spring application context
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        /* rootContext.register(ApplicationConfig.class, SecurityConfig.class);*/
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+        //Create the dispatcher servlet's Spring application context
+        AnnotationConfigWebApplicationContext servletAppContext = new AnnotationConfigWebApplicationContext();
+        servletAppContext.register(WebConfig.class);
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(servletAppContext);
+        // throw NoHandlerFoundException to controller ExceptionHandler.class. Used for <error-page> analogue
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        //register and map the dispatcher servlet
+        //note Dispatcher servlet with constructor arguments
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+        FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encoding-filter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
     }
+
 
 
 }
